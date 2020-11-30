5c2cbb66772ab547c31b4e0462a792d763fc0558
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-30 17:30:47 +0100

    INIZIALIZZAZIONE VUE E INIZIO LAVORO SU MEDIA QUERY (LAYOUT FINO A LIMITLESS BANNER))

--- js/script.js
+++ js/script.js
@@ -2,3 +2,35 @@
                   // !WHITE IMS \\
 // *********************************************** \\
 
+const iAccademy = new Vue ({
+
+    el: '#white-ims',
+
+    data: {
+        // Link navbar
+        navbarLinks: [
+            {
+                link: 'Courses',
+            },
+            {
+                link: 'Zoom',
+            },
+            {
+                link: 'Pages',
+            },
+            {
+                link: 'Bundles',
+            },
+            {   
+                link: 'Course formats',
+            },
+            {
+                link: 'Add course'
+            },
+            {
+                link: 'Demos'
+            }
+        ],
+    }
+
+ }); // <-- END VUE
